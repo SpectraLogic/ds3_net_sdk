ca05a86f79e9b98f53e090a3b8586b1b5f3df133
hansdude <hansk@spectralogic.com>
2014-04-16 13:30:28 -0600

    Created a simple installer for the PowerShell module.

---
+++ Ds3ClientInstaller/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+
+namespace Ds3ClientInstaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Ds3ClientInstaller.Ds3Client.zip"))
+            using (var zip = new ZipArchive(stream))
+            {
+                var myDocs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                var moduleDirectory = Path.Combine(myDocs, @"WindowsPowerShell\Modules\Ds3Client");
+                if (Directory.Exists(moduleDirectory))
+                {
+                    Directory.Delete(moduleDirectory, true);
+                }
+                zip.ExtractToDirectory(moduleDirectory);
+            }
+        }
+    }
+}
