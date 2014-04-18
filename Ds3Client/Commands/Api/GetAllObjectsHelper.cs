2c74cd62ed4ed05c9bbb581e17209153a9a60517
hansdude <hansk@spectralogic.com>
2014-04-18 10:02:22 -0600

    Updated the bulk read command line operation to do paging when getting object lists to prime.

---
+++ Ds3Client/Commands/Api/GetAllObjectsHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Ds3.Models;
+
+namespace Ds3Client.Commands.Api
+{
+    internal static class GetAllObjectsHelper
+    {
+        private const int _defaultMaxKeys = 1000;
+
+        public static IEnumerable<Ds3Object> GetAllObjects(Ds3.Ds3Client client, string bucketName, string keyPrefix, int? maxKeys)
+        {
+            var remainingKeys = maxKeys.HasValue ? maxKeys.Value : int.MaxValue;
+            var isTruncated = false;
+            string marker = null;
+            do
+            {
+                var request = new Ds3.Calls.GetBucketRequest(bucketName)
+                {
+                    Marker = marker,
+                    MaxKeys = Math.Min(remainingKeys, _defaultMaxKeys),
+                    Prefix = keyPrefix
+                };
+                using (var response = client.GetBucket(request))
+                {
+                    isTruncated = response.IsTruncated;
+                    marker = response.NextMarker;
+                    remainingKeys -= response.Objects.Count;
+                    foreach (var ds3Object in response.Objects)
+                    {
+                        yield return ds3Object;
+                    }
+                }
+            } while (isTruncated && remainingKeys > 0);
+        }
+    }
+}
--- Ds3Client/Commands/Api/GetObjectCommand.cs
+++ Ds3Client/Commands/Api/GetObjectCommand.cs
@@ -7,8 +7,6 @@ namespace Ds3Client.Commands.Api
     [Cmdlet(VerbsCommon.Get, DS3Nouns.Object)]
     public class GetObjectCommand : BaseApiCommand
     {
-        private const int _defaultMaxKeys = 1000;
-
         [Parameter(Position = 0, ValueFromPipelineByPropertyName = true, ValueFromPipeline = true, Mandatory = true)]
         public string BucketName { get; set; }
 
@@ -20,28 +18,10 @@ namespace Ds3Client.Commands.Api
 
         protected override void ProcessRecord()
         {
-            var client = CreateClient();
-            var remainingKeys = MaxKeys.HasValue ? MaxKeys.Value : int.MaxValue;
-            var isTruncated = false;
-            string marker = null;
-            do
+            foreach (var ds3Object in GetAllObjectsHelper.GetAllObjects(CreateClient(), BucketName, KeyPrefix, MaxKeys))
             {
-                var request = new Ds3.Calls.GetBucketRequest(BucketName) {
-                    Marker = marker,
-                    MaxKeys = Math.Min(remainingKeys, _defaultMaxKeys),
-                    Prefix = KeyPrefix
-                };
-                using (var response = client.GetBucket(request))
-                {
-                    isTruncated = response.IsTruncated;
-                    marker = response.NextMarker;
-                    remainingKeys -= response.Objects.Count;
-                    foreach (var ds3Object in response.Objects)
-                    {
-                        WriteObject(ds3Object);
-                    }
-                }
-            } while (isTruncated && remainingKeys > 0);
+                WriteObject(ds3Object);
+            }
         }
     }
 }
