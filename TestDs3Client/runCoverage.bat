113222d01924b6603a9bcc824dca743c04c80e95
hansdude <hansk@spectralogic.com>
2014-05-12 09:37:00 -0600

    Added test coverage report for the powershell client.

---
+++ TestDs3Client/runCoverage.bat
@@ -0,0 +1,2 @@
+..\..\..\packages\OpenCover.4.5.2506\OpenCover.Console.exe -target:runTests.bat -register:user -filter:+[Ds3Client]* 
+..\..\..\packages\ReportGenerator.1.9.1.0\reportgenerator.exe -reports:results.xml -targetdir:coverage
