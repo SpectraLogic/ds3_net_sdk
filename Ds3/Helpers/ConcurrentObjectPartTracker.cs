9fc0e0b26432fe5d9a8ef986fda2b4ef078ddb86
hansdude <hansk@spectralogic.com>
2014-09-03 13:40:52 -0600

    Added a concurrent object part tracker to wrap all calls with lock.

---
+++ Ds3/Helpers/ConcurrentObjectPartTracker.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Ds3.Helpers
+{
+    internal class ConcurrentObjectPartTracker : IObjectPartTracker
+    {
+        private readonly IObjectPartTracker _innerTracker;
+        private readonly object _lock = new object();
+
+        public ConcurrentObjectPartTracker(IObjectPartTracker innerTracker)
+        {
+            this._innerTracker = innerTracker;
+        }
+
+        public event Action<long> DataTransferred
+        {
+            add
+            {
+                lock (this._lock)
+                {
+                    this.DataTransferred += value;
+                }
+            }
+            remove
+            {
+                lock (this._lock)
+                {
+                    this.DataTransferred -= value;
+                }
+            }
+        }
+
+        public event Action Completed
+        {
+            add
+            {
+                lock (this._lock)
+                {
+                    this._innerTracker.Completed += value;
+                }
+            }
+            remove
+            {
+                lock (this._lock)
+                {
+                    this._innerTracker.Completed -= value;
+                }
+            }
+        }
+
+        public void CompletePart(ObjectPart partToRemove)
+        {
+            lock (this._lock)
+            {
+                this._innerTracker.CompletePart(partToRemove);
+            }
+        }
+    }
+}
