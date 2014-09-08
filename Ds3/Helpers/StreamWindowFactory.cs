ada7fb72836e75b03e4233924a083a806519b275
hansdude <hansk@spectralogic.com>
2014-09-08 11:26:57 -0600

    Created a stream window factory.
    This allows us to easily generate windowed streams and dispose the inner stream when needed.

---
+++ Ds3/Helpers/StreamWindowFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Ds3.Helpers
+{
+    internal class StreamWindowFactory : IDisposable
+    {
+        private bool _disposed;
+        private readonly Stream _stream;
+        private readonly ICriticalSectionExecutor _criticalSectionExecutor = new CriticalSectionExecutor();
+
+        public StreamWindowFactory(Stream stream)
+        {
+            this._stream = stream;
+        }
+
+        public Stream Get(long offset, long length)
+        {
+            return new WindowedStream(this._stream, this._criticalSectionExecutor, offset, length);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this._disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                this._stream.Dispose();
+            }
+
+            this._disposed = true;
+        }
+    }
+}
