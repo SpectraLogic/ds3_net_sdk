d657752cf5a7d2b81dd3132b2e0f566abf7ca89d
Ryan Moore <ryan.moore@greyrocksoft.com>
2015-12-03 10:27:49 -0700

    Adding function definition for buffered copy to

---
+++ Ds3/Lang/StreamsUtil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Ds3.Lang
+{
+    public static class StreamsUtil
+    {
+        public static void BufferedCopyTo(Stream src, Stream dst, Int32 bufferSize)
+        {
+        
+        }
+    }
+}
