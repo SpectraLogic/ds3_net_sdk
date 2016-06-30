c570e3c2b61fe55aae18cc8c8ce350911ee83574
Sharon Shabtai <sharons@spectralogic.com>
2016-06-30 17:17:36 -0600

    cleanup

--- Ds3/Runtime/Ds3RedirectLimitException.cs
+++ Ds3/Runtime/Ds3RedirectLimitException.cs
@@ -13,12 +13,6 @@
  * ****************************************************************************
  */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Ds3.Runtime
 {
     public class Ds3RedirectLimitException : Ds3RequestException
