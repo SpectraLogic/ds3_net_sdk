7546eb5f70e8f6cb6032dbb72ea675b23b4f8c14
Sharon Shabtai <sharons@spectralogic.com>
2016-07-29 17:44:34 -0600

    add test for null predicates

--- IntegrationTestDS3/IntegrationTestDS3Client.cs
+++ IntegrationTestDS3/IntegrationTestDS3Client.cs
@@ -1220,5 +1220,39 @@ namespace IntegrationTestDs3
                 Ds3TestUtils.DeleteBucket(_client, bucketName);
             }
         }
+
+        [Test]
+        public void TestNullFilterDs3Object()
+        {
+            const string bucketName = "TestNullFilterDs3Object";
+
+            try
+            {
+                _client.PutBucket(new PutBucketRequest(bucketName));
+                var stream = new MemoryStream(new byte[] {});
+                _client.PutObject(new PutObjectRequest(bucketName, "i_am_a_folder/", stream));
+
+                stream = new MemoryStream(new byte[] {});
+                _client.PutObject(new PutObjectRequest(bucketName, "i_am_a_zero_length_object", stream));
+
+                var objects = _helpers.ListObjects(bucketName);
+                Assert.AreEqual(2, objects.Count());
+
+                var objectsFiltered = Ds3ClientHelpers.FilterDs3Objects(objects, null);
+                Assert.AreEqual(2, objectsFiltered.Count());
+
+                objectsFiltered = Ds3ClientHelpers.FilterDs3Objects(objects, Ds3ClientHelpers.FolderFilterPredicate,
+                    null);
+                Assert.AreEqual(1, objectsFiltered.Count());
+
+                objectsFiltered = Ds3ClientHelpers.FilterDs3Objects(objects, null,
+                    Ds3ClientHelpers.ZeroLengthFilterPredicate);
+                Assert.AreEqual(0, objectsFiltered.Count());
+            }
+            finally
+            {
+                Ds3TestUtils.DeleteBucket(_client, bucketName);
+            }
+        }
     }
 }
