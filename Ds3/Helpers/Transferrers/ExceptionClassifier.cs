db40d4f55924718ee7473cfde54b9c9529af7b6b
Sharon Shabtai <sharons@spectralogic.com>
2016-09-09 15:32:09 -0600

    Enhance best effort to distinguish between 'error' & 'failure'

---
+++ Ds3/Helpers/Transferrers/ExceptionClassifier.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * ******************************************************************************
+ *   Copyright 2014-2016 Spectra Logic Corporation. All Rights Reserved.
+ *   Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ *   this file except in compliance with the License. A copy of the License is located at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file.
+ *   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *   specific language governing permissions and limitations under the License.
+ * ****************************************************************************
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Ds3.Runtime;
+
+namespace Ds3.Helpers.Transferrers
+{
+    internal class ExceptionClassifier
+    {
+        private static readonly IList<Type> RecoverableExceptions = new List<Type>
+        {
+            typeof(Ds3ContentLengthNotMatch),
+            typeof(WebException),
+            typeof(IOException)
+        };
+
+        public static bool IsRecoverableException(Exception t)
+        {
+            return RecoverableExceptions.Contains(t.GetType());
+        }
+
+        public static bool IsUnrecoverableException(Exception t) {
+            return !IsRecoverableException(t);
+        }
+    }
+}
