1aa402e2463f62d486cefebdde6f9142c98be80f
Sharon Shabtai <sharons@spectralogic.com>
2016-10-10 11:58:18 -0600

    upgrade .NET version to 4.5.2

--- TestDs3/runCoverage.bat
+++ TestDs3/runCoverage.bat
@@ -1,2 +1,11 @@
+
+
+
+
+
+
+
+
+
 ..\..\..\packages\OpenCover.4.5.2506\OpenCover.Console.exe -target:runTests.bat -register:user -filter:+[ds3_net_sdk]* 
 ..\..\..\packages\ReportGenerator.1.9.1.0\reportgenerator.exe -reports:results.xml -targetdir:coverage
