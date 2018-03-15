5a2a314491903bb27f52d802c9f0f0f0590c0731
Sharon Shabtai <sharons@spectralogic.com>
2018-03-15 12:33:03 -0600

    update the version

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -18,13 +18,13 @@ using System.Reflection;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.0.2.0")]
-[assembly: AssemblyFileVersion("4.0.2.0")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
