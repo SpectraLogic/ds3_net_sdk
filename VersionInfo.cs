b8c22cd03aee5495fc8cdbf8ecff1fdd70d0c92f
Sharon Shabtai <sharons@spectralogic.com>
2018-05-04 10:46:59 -0600

    update version to v4.1.2

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -26,5 +26,5 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.1.1.0")]
-[assembly: AssemblyFileVersion("4.1.1.0")]
+[assembly: AssemblyVersion("4.1.2.0")]
+[assembly: AssemblyFileVersion("4.1.2.0")]
