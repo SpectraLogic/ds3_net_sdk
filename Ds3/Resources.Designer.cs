17dee844dc2fda156cc00a6e11d11cf561f50b40
Sharon Shabtai <sharons@spectralogic.com>
2018-05-11 16:55:41 -0600

    fix a typo

--- Ds3/Resources.Designer.cs
+++ Ds3/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Ds3 {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -88,7 +88,7 @@ namespace Ds3 {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The Content length ({0}) not match the number of byte read ({1}).
+        ///   Looks up a localized string similar to The Content length ({0}) not match the number of bytes read ({1}).
         /// </summary>
         internal static string ContentLengthNotMatch {
             get {
