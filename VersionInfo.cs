4c32dd8bc2cbe99135936784f1ea1911839f85b2
Sharon Shabtai <sharons@spectralogic.com>
2019-04-30 16:12:22 -0600

    Update version (#338)
    * Support sparse file
    
    * Bump the version

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -26,5 +26,5 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.4.2.2")]
-[assembly: AssemblyFileVersion("3.4.2.2")]
+[assembly: AssemblyVersion("3.4.2.3")]
+[assembly: AssemblyFileVersion("3.4.2.3")]
